747dae7d6e72d2d74866dca9c9f834a6219cf553
Jake Kandell <jakekandell03@gmail.com>
2019-08-01 16:07:45 -0400

    added availableStats.py

---
+++ availableStats.py
@@ -0,0 +1,10 @@
+# Dictionary holding stats that are analyzed for each game and their type
+# Update if new stats are added
+
+availableStats = {'W_PCT':'Base',
+                  'REB':'Base',
+                  'TOV':'Base',
+                  'PLUS_MINUS':'Base',
+                  'OFF_RATING':'Advanced',
+                  'DEF_RATING':'Advanced',
+                  'TS_PCT':'Advanced'}
