d44a605b3e8f9dcd9e2a5dc9f78a72379d558026
Jake Kandell <jakekandell03@gmail.com>
2019-10-11 01:14:51 -0400

    added comments

--- configureCWD.py
+++ configureCWD.py
@@ -1,5 +1,8 @@
+# configureCWD.py - Sets current working directory relative to where program folder is located
+
 import os
 
+# Sets current working directory relative to where program folder is located
 def setCurrentWorkingDirectory(directoryName):
 
     programDirectory = os.path.dirname(os.path.abspath(__file__))
